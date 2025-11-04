d5e160e4dd48528f9f2bf58fd044bd7e2118183c
Kamil-Salih <C00307549@setu.ie>
2025-11-04 17:41:30 +0000

    update

---
+++ lab 3-2/time_and_date_tricks.py
@@ -0,0 +1,23 @@
+import time, datetime
+
+print(time.time())  # current Unix timestamp
+
+print(datetime.datetime.now())
+print(datetime.datetime(2025, 10, 12, 14, 45, 2))
+print(datetime.datetime.fromtimestamp(1700000000))
+print(datetime.datetime(2023, 11, 14, 22, 13, 20))
+
+'''
+Exercise:
+Convert your birthday to a Unix timestamp.
+
+Convert 1700000000 back to a readable date.
+
+'''
+
+print("\n")
+
+birthday= datetime.datetime(2006, 6, 11, 0, 0, 0)
+print(birthday.timestamp())
+
+print(datetime.datetime.fromtimestamp(1700000000))
